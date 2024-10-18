d2cd5a121b843aaf0ba346af5f2012e3739b7560
EXCELINDIA\robin.perera <robin.perera@excelindia.com>
2024-10-18 15:16:19 +0530

    Initail commit

---
+++ backend/JavaChatbot/src/main/java/com/JavaChatbot/JavaChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.JavaChatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChatbotApplication.class, args);
+	}
+
+}
---
+++ backend/JavaChatbot/src/test/java/com/JavaChatbot/JavaChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaChatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
